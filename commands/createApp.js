89e795ec2c0a5e6014983b95634380dac37a88d3
Tejus Raghavendra <tejusraghavendra09@gmail.com>
2023-11-12 20:54:57 +0530

    feat: added git integration for create app

--- commands/createApp.js
+++ commands/createApp.js
@@ -1,5 +1,8 @@
 import path from "path";
-    
+import inquirer from "inquirer";
+import fse from "fs-extra";
+import { gitHelper } from "../helper.js";
+
 const commandCreateApp = {
     command: "create app <path>",
     describe: "Create an app from a template",
@@ -11,6 +14,23 @@ const commandCreateApp = {
         try {
             await fse.ensureDir(destinationPath);
             await fse.copy(templatePath, destinationPath);
+            const { shouldCommit } = await inquirer.prompt({
+                type: "confirm",
+                name: "shouldCommit",
+                message: "Do you want to commit these changes?",
+            });
+
+            if (shouldCommit) {
+                const { commitMessage } = await inquirer.prompt({
+                    type: "input",
+                    name: "commitMessage",
+                    message: "Enter the commit message:",
+                    default: "Initial commit ✨",
+                });
+                await gitHelper.commitChanges(destinationPath, commitMessage);
+                console.log("Committing changes with message:", commitMessage);
+            }
+
             console.log("Template copied successfully!");
         } catch (err) {
             console.error("Error copying template:", err);
--- helper.js
+++ helper.js
@@ -1,9 +1,9 @@
 import path from "path";
 import fs from "fs";
 import ejs from "ejs";
+import simpleGit from "simple-git";
 
 export const ejsHelper = async ({ templatePath, fName, data, outFilePath }) => {
-
     fs.mkdir(outFilePath, { recursive: true }, (err) => {
         if (err) {
             if (err.code === "EEXIST") {
@@ -45,3 +45,20 @@ export const ejsHelper = async ({ templatePath, fName, data, outFilePath }) => {
     });
 };
 
+export const gitHelper = async ({ path, commitMessage = "Initial commit ✨" }) => {
+    const git = simpleGit(path);
+    try {
+        // Initialize Git repository
+        await git.init();
+
+        // Add all files
+        await git.add(".");
+
+        // Make the initial commit
+        await git.commit(commitMessage);
+
+        console.log("Git repository initialized and initial commit made.");
+    } catch (error) {
+        console.error("Error initializing Git repository:", error);
+    }
+};
